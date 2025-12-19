51b44c641226ca5cc7edeba91b23753cbde27e7a
cryorepository <cryorepository@protonmail.com>
2025-12-19 16:59:44 +1100

    lessen webhook validation

--- src/index.ts
+++ src/index.ts
@@ -155,7 +155,7 @@ app.post("/refreshAll/:password", async (req: Request, res: Response): Promise<v
 
 
 
-const UserMentionSchema = z.object({
+/*const UserMentionSchema = z.object({
   id_str: z.string(),
   indices: z.array(z.number()).length(2),
   name: z.string(),
@@ -272,6 +272,118 @@ const TweetSchema = z.object({
   retweeted_tweet: z.null(),
   isLimitedReply: z.boolean(),
   article: z.null(),
+});*/
+
+// -----------------------------------------------------------------------------
+/*const UserMentionSchema = z.object({
+  id_str: z.string(),
+  indices: z.array(z.number()).length(2),
+  name: z.string(),
+  screen_name: z.string(),
+});
+
+const EntitiesSchema = z.object({
+  user_mentions: z.array(UserMentionSchema),
+});
+
+const SymbolSchema = z.object({
+  indices: z.array(z.number()).length(2),
+  text: z.string(),
+});
+
+const ProfileBioEntitiesDescriptionSchema = z.object({
+  symbols: z.array(SymbolSchema),
+});
+
+const ProfileBioEntitiesSchema = z.object({
+  description: ProfileBioEntitiesDescriptionSchema,
+});
+
+const ProfileBioSchema = z.object({
+  description: z.string(),
+  entities: ProfileBioEntitiesSchema,
+});
+
+const UserEntitiesDescriptionSchema = z.object({
+  urls: z.array(z.unknown()).length(0),
+});
+
+const UserEntitiesSchema = z.object({
+  description: UserEntitiesDescriptionSchema,
+  url: z.object({}),
+});*/
+
+const AuthorSchema = z.object({
+  type: z.literal('user'),
+  userName: z.string(),
+  url: z.string(),
+  //twitterUrl: z.string(),
+  //id: z.string(),
+  name: z.string(),
+  //isVerified: z.boolean(),
+  //isBlueVerified: z.boolean(),
+  //verifiedType: z.null(),
+  profilePicture: z.string(),
+  /*coverPicture: z.string(),
+  description: z.string(),
+  location: z.string(),
+  followers: z.number(),
+  following: z.number(),
+  status: z.string(),
+  canDm: z.boolean(),
+  canMediaTag: z.boolean(),
+  createdAt: z.string(),
+  entities: UserEntitiesSchema,
+  fastFollowersCount: z.number(),
+  favouritesCount: z.number(),
+  hasCustomTimelines: z.boolean(),
+  isTranslator: z.boolean(),
+  mediaCount: z.number(),
+  statusesCount: z.number(),
+  withheldInCountries: z.array(z.unknown()).length(0),
+  affiliatesHighlightedLabel: z.object({}),
+  possiblySensitive: z.boolean(),
+  pinnedTweetIds: z.array(z.unknown()).length(0),
+  profile_bio: ProfileBioSchema,
+  isAutomated: z.boolean(),
+  automatedBy: z.null(),*/
+});
+
+const MediaItemSchema = z.object({
+  display_url: z.string(),
+  expanded_url: z.string(),
+  media_key: z.string(),
+  media_url_https: z.string(),
+  type: z.string(),
+  url: z.string(),
+});
+
+const TweetSchema = z.object({
+  type: z.literal('tweet'),
+  id: z.string(),
+  url: z.string(),
+  text: z.string(),
+  viewCount: z.number(),
+  createdAt: z.string(),
+  lang: z.string(),
+  bookmarkCount: z.number(),
+  isReply: z.boolean(),
+  //inReplyToId: z.string(),
+  //conversationId: z.string(),
+  //displayTextRange: z.array(z.number()).length(2),
+  //inReplyToUserId: z.string(),
+  //inReplyToUsername: z.string(),
+  author: AuthorSchema,
+  extendedEntities: z.object({
+    media: z.array(MediaItemSchema)
+  }),
+  /*card: z.null(),
+  place: z.object({}),
+  entities: EntitiesSchema,
+  quoted_tweet: z.null(),
+  retweeted_tweet: z.null(),
+  isLimitedReply: z.boolean(),
+  article: z.null(),*/
 });
 
 // TEMP
